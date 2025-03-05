3e3f20375c63ec98815d585e17357210b96fd9ee
@AdamTheThirst <chatbotsadmn@gmail.com>
2025-03-05 21:20:27 +0300

    update

--- merge_lists.py
+++ merge_lists.py
@@ -2,6 +2,8 @@
 
 from random import randint
 
+# version 1
+
 lst_1 = [randint(0, 50) for i in range(10)]
 lst_2 = [randint(30, 70) for i in range(12)]
 
@@ -11,14 +13,10 @@ lst_2.sort()
 if lst_1 < lst_2:
     lst_1, lst_2 = lst_2, lst_1
 
-len_1 = len(lst_1)
-len_2 = len(lst_2)
-
 result = list()
 
 print(lst_1)
 print(lst_2)
-
 
 flag = True
 
@@ -36,5 +34,40 @@ while flag:
 
 if len(lst_1) > 0:
     result.extend(lst_1)
+
+print(result)
+
+
+# version 2
+
+lst_1 = [randint(0, 50) for i in range(randint(10, 15))]
+lst_2 = [randint(30, 70) for i in range(randint(10, 15))]
+
+lst_1.sort()
+lst_2.sort()
+
+if lst_1 < lst_2:
+    lst_1, lst_2 = lst_2, lst_1
+
+print()
+print(lst_1)
+print(lst_2)
+
+
+result = list()
+
+j = 0
+
+for i in range(len(lst_1)):
+    if j >= len(lst_2):
+        result.extend(lst_1[i-1:-1])
+        break
+    for _ in range(len(lst_2)-j):
+        if lst_1[i] >= lst_2[j]:
+            result.append(lst_2[j])
+            j += 1
+        else:
+            result.append(lst_1[i])
+            break
 
 print(result)
